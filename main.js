fef49f6190e9ec34f2f08442d1e783c4efa3d96d
vanko <ivan.tym4ak@gmail.com>
2024-01-10 11:59:15 +0200

    create users

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ app.get('/users', async (req, res) => {
         const users = await read();
         res.status(200).json({data: users});
     } catch (e) {
-        res.status(400).json('Error')
+        res.status(400).json(e.message);
     }
 });
 
@@ -31,21 +31,88 @@ app.get('/users/:id', async (req, res) => {
         }
         res.status(200).json({data: users[index]})
     } catch (e) {
-        res.status(400).json('Error')
+        res.status(400).json(e.message);
     }
 })
 
-app.post('/users', (req, res) => {
-    const newUser = req.body;
+app.post('/users', async (req, res) => {
+    try {
+        const {email, name, age} = req.body;
 
-    res.status(200).json(newUser);
+        if (!age || !Number.isInteger(age) || age <= 0 || age > 100) {
+            throw new Error('Wrong age');
+        }
+        if (!email || !email.includes('@')) {
+            throw new Error('Wrong email');
+        }
+        if (!name || name.length <= 3) {
+            throw new Error('Wrong name');
+        }
+        const users = await read();
+        const newUser = {id: users[users.length - 1].id + 1, email, name, age};
+        users.push(newUser);
+        await write(users);
+
+        res.status(201).json({data: newUser});
+    } catch (e) {
+        res.status(400).json(e.message);
+    }
 });
 
-app.put('/users/:id', (req, res) => {
+app.put('/users/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const {email, age, name} = req.body;
 
+        if (!Number.isInteger(id)) {
+            throw new Error('Wrong ID param')
+        }
+        if (!age || !Number.isInteger(age) || age <= 0 || age > 100) {
+            throw new Error('Wrong age');
+        }
+        if (!email || !email.includes('@')) {
+            throw new Error('Wrong email');
+        }
+        if (!name || name.length <= 3) {
+            throw new Error('Wrong name');
+        }
+
+        const users = await read();
+        const user = users.find(user => user.id === id);
+
+        if (!user) {
+            throw new Error('User not found')
+        }
+        user.name = name;
+        user.age = age;
+        user.email = email;
+
+        await write(users);
+        res.status(201).json({data: user});
+    } catch (e) {
+        res.status(400).json(e.message);
+    }
 });
 
-app.delete('/users/:id', (req, res) => {
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+
+        if (!Number.isInteger(id)) {
+            throw new Error('Wrong ID param')
+        }
+        const users = await read();
+        const index = users.findIndex(user => user.id === id);
+
+        if (index === -1) {
+            throw new Error('User not found')
+        }
+        users.splice(index, 1);
+        await write(users);
+        res.status(204).json({message: 'User deleted'});
+    } catch (e) {
+        res.status(400).json(e.message);
+    }
 
 });
 
--- user.service.js
+++ user.service.js
@@ -1,14 +1,15 @@
 const fs = require('node:fs/promises');
 const path = require('node:path');
 
+const pathToDb = path.join(process.cwd(), 'users-db.json');
+
 const read = async () => {
-    const pathToDb = path.join(process.cwd(), 'users-db.json');
     const users = await fs.readFile(pathToDb, {encoding: 'utf-8'});
     return JSON.parse(users);
 }
 
-const write = async () => {
-
+const write = async (users) => {
+    await fs.writeFile(pathToDb, JSON.stringify(users));
 }
 
 module.exports = {read, write};
