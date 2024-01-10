104fcf7556aca3af3cd403de561cfb5513d904e0
vanko <ivan.tym4ak@gmail.com>
2024-01-10 12:16:23 +0200

    fixed first commit main

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const {read, write} = require('./user.service');
 
 const app = express();
 const PORT = 5001;
@@ -7,38 +6,16 @@ const PORT = 5001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/users', async (req, res) => {
-    try {
-        const users = await read();
-        res.status(200).json({data: users});
-    } catch (e) {
-        res.status(400).json('Error')
-    }
+app.get('/users', (req, res) => {
+
 });
 
-app.get('/users/:id', async (req, res) => {
-    try {
-        const id = Number(req.params.id);
+app.get('/users/:id', (req, res) => {
 
-        if (!Number.isInteger(id)) {
-            throw new Error('Wrong ID param')
-        }
-        const users = await read();
-        const index = users.findIndex(user => user.id === id);
-
-        if (index === -1) {
-            throw new Error('User not found')
-        }
-        res.status(200).json({data: users[index]})
-    } catch (e) {
-        res.status(400).json('Error')
-    }
 })
 
 app.post('/users', (req, res) => {
-    const newUser = req.body;
 
-    res.status(200).json(newUser);
 });
 
 app.put('/users/:id', (req, res) => {
